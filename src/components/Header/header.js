8fe920ee2e63dbb3feb8fad2129f24514121b98e
DCCA <dcca12@gmail.com>
2020-03-10 10:14:20 -0700

    add style for the header, add create footer folder structure and files

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,10 +1,11 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import style from "./header.scss"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <div>
+    <div className='logo'>
 	<h1>DC<br />CA</h1>
     </div>
     <div>
